d66f133ec7adbd49e0e5f3ee1611311db719295d
dilpreet137 <139755043+dilpreet137@users.noreply.github.com>
2026-04-13 17:39:06 +0530

    swagger generation added (#2)

--- CoreAPI/Phoenix.CoreAPI/Program.cs
+++ CoreAPI/Phoenix.CoreAPI/Program.cs
@@ -9,11 +9,16 @@ builder.ConfigureLogging();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 if (isDevEnvironment)
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(opt => opt.SwaggerEndpoint("/openapi/v1.json", "Phoenix.CoreAPI v1"));
 }
 
 app.UseSerilogRequestLogging();
